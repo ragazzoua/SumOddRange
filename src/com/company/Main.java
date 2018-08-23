f53ff5c7c5fa58823e73d81143c0a3c0cb15dba4
Serhii A. Miroshnychenko <s.a.miroshnychenko@globallogic.com>
2018-08-23 10:20:34 +0300

    SumOddRange Udemy

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import static com.company.SumOddRange.sumOdd;
+
+/**
+ * created by s.a.miroshnychenko 8/23/2018
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(sumOdd(-4, 6));
+    }
+}
